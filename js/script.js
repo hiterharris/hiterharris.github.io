ce0b459aea337f16f856f9a354dbb5e9d85e69c2
hiterharris <HiterHarrisIV@Hs-MacBook-Pro.local>
2017-04-18 13:18:52 -0400

    hover states and scrollTop on js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// $(document).ready(function (){
+//            $("#click").click(function (){
+//                $('html, body').animate({
+//                    scrollTop: $("#my-work").offset().top
+//                }, 500);
+//            });
+//        });
+
+$(document).ready(function(){
+  $("#click").click(function (){
+    $('html, body').animate({
+      scrollTop: $("#my-work").offset().top
+    }, 500);
+  });
+});
