76efd8afc88f6ccaabc5e02fbaba12c58b2f2163
hiterharris <HiterHarrisIV@Hs-MacBook-Pro.local>
2017-04-18 13:56:24 -0400

    scroll button added in js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ $(document).ready(function(){
     }, 500);
   });
 });
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 10) {
+            $("#btnTop").fadeIn();
+        } else {
+            $("#btnTop").fadeOut();
+        }
+  });
+
+
+  $('#btnTop').click(function(){
+    window.scrollTo(0,0);
+  });
+});
