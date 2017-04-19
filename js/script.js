85e576bc045473dbdbdb87d14e8cebdc6db5f85a
hiterharris <HiterHarrisIV@Hs-MacBook-Pro.local>
2017-04-19 09:36:25 -0400

    morning commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// $(document).ready(function (){
-//            $("#click").click(function (){
-//                $('html, body').animate({
-//                    scrollTop: $("#my-work").offset().top
-//                }, 500);
-//            });
-//        });
-
 $(document).ready(function(){
   $("#click").click(function (){
     $('html, body').animate({
